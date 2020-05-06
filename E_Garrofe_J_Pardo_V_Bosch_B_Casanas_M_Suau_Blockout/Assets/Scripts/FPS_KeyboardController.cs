eeba9ed461a72fa62ae7ee44fabc352344e78be0
MayKoder <sg.miquel@gmail.com>
2020-05-06 22:01:21 +0200

    FPS Added

---
+++ E_Garrofe_J_Pardo_V_Bosch_B_Casanas_M_Suau_Blockout/Assets/Scripts/FPS_KeyboardController.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+
+public class FPS_KeyboardController : MonoBehaviour
+{
+
+    [Header("Movement Settings")]
+    public Vector3 movementSpeed;
+    public float speedLimits;
+    Rigidbody rb_body;
+    public bool isGrounded = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb_body = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        if(rb_body.velocity.magnitude <= speedLimits || !isGrounded)
+        {
+            //Forward movement
+            if (Input.GetKey(KeyCode.W))
+            {
+                rb_body.velocity += transform.forward * movementSpeed.x * Time.deltaTime;
+            }
+
+            //Backwards movement
+            if (Input.GetKey(KeyCode.S))
+            {
+                rb_body.velocity += (transform.forward * -1) * movementSpeed.x * Time.deltaTime;
+            }
+
+            //Right movement
+            if (Input.GetKey(KeyCode.D))
+            {
+                rb_body.velocity += transform.right * movementSpeed.z * Time.deltaTime;
+            }
+
+            //Left movement
+            if (Input.GetKey(KeyCode.A))
+            {
+                rb_body.velocity += (transform.right * -1) * movementSpeed.z * Time.deltaTime;
+            }
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.layer == LayerMask.NameToLayer("Default"))
+        {
+            isGrounded = true;
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Default"))
+        {
+            isGrounded = false;
+        }
+    }
+
+}
---
+++ E_Garrofe_J_Pardo_V_Bosch_B_Casanas_M_Suau_Blockout/Assets/Scripts/FPS_MouseController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class FPS_MouseController : MonoBehaviour
+{
+
+    [Header("Camera Speed Settings")]
+    public Vector2 CameraSpeed;
+    public Camera camera;
+
+    //Body settings
+    Transform body;
+    float deltaTime = 0.0f;
+
+    private void Awake()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        body = this.transform;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Get mouse position increments
+        float lookHor = Input.GetAxis("Mouse X");
+        float lookVert = Input.GetAxis("Mouse Y");
+
+        body.Rotate(0, lookHor * CameraSpeed.x, 0);
+
+        Debug.Log(camera.transform.rotation.eulerAngles);
+        camera.transform.Rotate(-lookVert * CameraSpeed.y, 0, 0);
+    }
+}
